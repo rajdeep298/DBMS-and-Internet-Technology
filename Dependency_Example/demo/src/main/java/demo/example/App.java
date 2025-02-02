7a5e942746e389b80eb86f7db8a15c431ac48afa
rajdeep298 <rajdeepkundu2983@gmail.com>
2025-02-02 17:09:46 +0530

    some mild changes

--- Dependency_Example/demo/src/main/java/demo/example/App.java
+++ Dependency_Example/demo/src/main/java/demo/example/App.java
@@ -14,18 +14,18 @@ public class App {
 //		System.out.println(swift.getCar());
 //		System.out.println(Corolla.getCar());
 //		
-		Car mycar = new Swift();
-		System.out.println(mycar.getCar());
-		
-		mycar = new Corolla();
-		System.out.println(mycar.getCar()); //This is known as tight coupling
-		
-//		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-//		Car mycar = context.getBean("mySwift", Car.class);
+//		Car mycar = new Swift();
 //		System.out.println(mycar.getCar());
 //		
-//		mycar = context.getBean("myCorolla", Car.class);
-//		System.out.println(mycar.getCar());
+//		mycar = new Corolla();
+//		System.out.println(mycar.getCar()); //This is known as tight coupling
+		
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		Car mycar = context.getBean("mySwift", Car.class);
+		System.out.println(mycar.getCar());
+		
+		mycar = context.getBean("myCorolla", Car.class);
+		System.out.println(mycar.getCar());
 	}
 	
 }
